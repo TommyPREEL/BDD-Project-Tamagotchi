01f029a3859f52805ecb04067c7296a5163ef019
MathiasGenibrel <mathias.geni@gmail.com>
2023-01-08 19:36:55 +0100

    Create procedure / trigger for actions

--- sql/sql.sql
+++ sql/sql.sql
@@ -14,16 +14,26 @@ CREATE TABLE IF NOT EXISTS tamagotchis
 (
   id_users      TINYINT UNSIGNED,
   id            TINYINT UNSIGNED AUTO_INCREMENT,
-  nom           VARCHAR(50) NOT NULL,
+  name          VARCHAR(50) NOT NULL,
   hungry        TINYINT     NOT NULL DEFAULT 70,
   thirsty       TINYINT     NOT NULL DEFAULT 70,
   sleep         TINYINT     NOT NULL DEFAULT 70,
   boredom       TINYINT     NOT NULL DEFAULT 70,
   level         TINYINT     NOT NULL DEFAULT 1,
-  nb_action     TINYINT     NOT NULL DEFAULT 0,
+  nb_action     SMALLINT    NOT NULL DEFAULT 0,
   creation_date DATETIME    NOT NULL DEFAULT NOW(),
   dead_date     DATETIME,
   death_reason  VARCHAR(50),
   PRIMARY KEY (id),
   FOREIGN KEY (id_users) REFERENCES users (id)
 );
+
+CREATE TABLE IF NOT EXISTS actions
+(
+  id_tamagotchis TINYINT UNSIGNED,
+  id             TINYINT UNSIGNED AUTO_INCREMENT        NOT NULL,
+  type           ENUM ('eat', 'drink', 'sleep', 'play') NOT NULL,
+  creation_date  DATETIME                               NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_tamagotchis) REFERENCES tamagotchis (id)
+)
