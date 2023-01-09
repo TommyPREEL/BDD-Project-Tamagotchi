2abb631edda880bc0efd7de7e6651825faa2f529
MathiasGenibrel <mathias.geni@gmail.com>
2023-01-10 00:36:28 +0100

    Fix and Feat SQL Procedure / Trigger / Function

--- sql/procedures/create_tamagotchi.sql
+++ sql/procedures/create_tamagotchi.sql
@@ -1,5 +1,3 @@
-DELIMITER //
-
 CREATE PROCEDURE create_tamagotchi(IN user VARCHAR(50), IN tamagotchi_name VARCHAR(50))
 BEGIN
   DECLARE db_user_account_name VARCHAR(50);
@@ -10,8 +8,6 @@ BEGIN
     SIGNAL SQLSTATE '40001' SET MESSAGE_TEXT = 'User account not exist';
   END IF;
 
-  INSERT INTO tamagotchis (id_users, nom)
+  INSERT INTO tamagotchis (id_users, name)
   VALUES (db_user_account_id, tamagotchi_name);
 END;
-
-DELIMITER ;
--- sql/views/dead_tamagotchis.sql
+++ sql/views/dead_tamagotchis.sql
@@ -1,9 +1,10 @@
 CREATE VIEW dead_tamagotchis AS
-SELECT username,
-       nom,
-       dead_date,
-       death_reason,
-       level
+SELECT name,
+       reason,
+       tamagotchis.creation_date as creation_date,
+       deaths.creation_date      as death_date
+       -- TODO : MISSING LEVEL OF TAMAGOTCHI
 FROM tamagotchis
-       JOIN users u on u.id = tamagotchis.id_users
-WHERE dead_date IS NOT NULL;
+       JOIN users on users.id = tamagotchis.id_users
+       LEFT JOIN deaths on tamagotchis.id = deaths.id_tamagotchis
+WHERE is_alive(tamagotchis.id) IS FALSE;
