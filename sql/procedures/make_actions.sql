173ce3c144467d4581340819aeb6c6e609c8d7de
MathiasGenibrel <mathias.geni@gmail.com>
2023-01-09 21:40:45 +0100

    Use Prettier to format procedure

--- sql/procedures/make_actions.sql
+++ sql/procedures/make_actions.sql
@@ -1,5 +1,6 @@
-CREATE PROCEDURE make_action(IN tamagotchi_id TINYINT,
-                             IN action ENUM ('eat', 'drink', 'sleep', 'play')
+CREATE PROCEDURE make_action(
+  IN tamagotchi_id TINYINT,
+  IN action ENUM ('eat', 'drink', 'sleep', 'play')
 )
 BEGIN
   INSERT INTO actions (id_tamagotchis, type) VALUE (tamagotchi_id, action);
