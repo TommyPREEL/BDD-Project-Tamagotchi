d5231f583d3944756b07b5b4f2f019ce646df747
MathiasGenibrel <mathias.geni@gmail.com>
2023-01-10 21:12:30 +0100

    ✨ Create Each action procedure

--- sql/procedures/make_actions.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE make_action(
-  IN tamagotchi_id TINYINT,
-  IN action ENUM ('eat', 'drink', 'sleep', 'play')
-)
-BEGIN
-  INSERT INTO actions (id_tamagotchis, type) VALUE (tamagotchi_id, action);
-END;
