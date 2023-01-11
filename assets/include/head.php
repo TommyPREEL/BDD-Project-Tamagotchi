74d64feb2e04b5de23a047bb20df7b36143a9913
NyckolasP <nicolas@allforweb.fr>
2023-01-11 18:20:49 +0100

    Fix css

--- assets/include/head.php
+++ assets/include/head.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <link href="../assets/style/style.css" rel="stylesheet">
+        <link href="assets/style/style.css" rel="stylesheet">
     </head>
 
     <body>
