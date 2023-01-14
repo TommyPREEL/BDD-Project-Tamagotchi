e41ba0422a47b18f201e6f7940df8345153ec3ec
NyckolasP <nicolas@allforweb.fr>
2023-01-14 16:00:40 +0100

    Fix alive procédure

--- assets/sql/views/alive_tamagotchis.sql
+++ assets/sql/views/alive_tamagotchis.sql
@@ -12,4 +12,4 @@ FROM tamagotchis
        JOIN users on users.id = tamagotchis.id_users
        LEFT JOIN deaths on tamagotchis.id = deaths.id_tamagotchis
 WHERE is_alive(tamagotchis.id) IS TRUE
-ORDER BY DESC tamagotchis.creation_date;
+ORDER BY tamagotchis.creation_date DESC;
